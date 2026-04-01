37b920e0adf5f83089b3a1637bdfe9341877d371
guilhermediasmacedo <guilhermedias.macedo@ucsal.edu.br>
2026-04-01 15:50:44 -0300

    Update and rename src/main/java/br/com/ucsal/olimpiadas/App.java to src/main/java/br/com/ucsal/olimpiadas /controller/app.java

---
+++ src/main/java/br/com/ucsal/olimpiadas /controller/app.java
@@ -0,0 +1,43 @@
+package br.com.ucsal.olimpiadas;
+
+import br.com.ucsal.olimpiadas.model.BancoDeDados;
+import br.com.ucsal.olimpiadas.model.Prova;
+import br.com.ucsal.olimpiadas.model.QuestaoMultiplaEscolha;
+import br.com.ucsal.olimpiadas.view.Menu;
+
+public class App {
+
+	public static void main(String[] args) {
+		seed();
+		Menu.iniciar();
+	}
+
+	static void seed() {
+		var prova = new Prova();
+		prova.setId(BancoDeDados.proximaProvaId++);
+		prova.setTitulo("Olimpíada 2026 • Nível 1 • Prova A");
+		BancoDeDados.provas.add(prova);
+
+		var q1 = new QuestaoMultiplaEscolha();
+		q1.setId(BancoDeDados.proximaQuestaoId++);
+		q1.setProvaId(prova.getId());
+
+		q1.setEnunciado("""
+				Questão 1 — Mate em 1.
+				É a vez das brancas.
+				Encontre o lance que dá mate imediatamente.
+				""");
+
+		q1.setFenInicial("6k1/5ppp/8/8/8/7Q/6PP/6K1 w - - 0 1");
+		q1.setAlternativas(new String[] {
+				"A) Qh7#",
+				"B) Qf5#",
+				"C) Qc8#",
+				"D) Qh8#",
+				"E) Qe6#"
+		});
+		q1.setAlternativaCorreta('C');
+
+		BancoDeDados.questoes.add(q1);
+	}
+}
